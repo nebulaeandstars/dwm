57ca9d16f5b0175a6c441a0df049d5305fc7e8c8
Laura Powles <nebulaeandstars@gmail.com>
2021-03-21 22:34:23 +1100

    added 'resizecorners' patch

--- config.def.h
+++ config.def.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 0;        /* border pixel of windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -19,7 +19,7 @@ static const char *fonts[] = {
 };
 static const char dmenufont[]       = "monospace:size=14";
 static const char col_gray1[]       = "#2d2d2d";
-static const char col_gray2[]       = "#616161";
+static const char col_gray2[]       = "#4f4f4f";
 static const char col_gray3[]       = "#ffffff";
 static const char col_gray4[]       = "#2d2d2d";
 static const char col_cyan[]        = "#b4eded";
