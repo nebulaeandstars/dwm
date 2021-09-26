30c7bfabd013c3d001c93ed31df80dc57e7858b1
Laura Powles <laura@laurapowles.com>
2021-09-26 19:46:58 +1000

    added 'scratchpad' patch

--- config.def.h
+++ config.def.h
@@ -96,6 +96,8 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *layoutmenu_cmd = "layoutmenu";
+static const char scratchpadname[] = "scratchpad";
+static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
 
 #include "movestack.c"
 static Key keys[] = {
@@ -116,6 +118,8 @@ static Key keys[] = {
     { MODKEY,                       XK_space,  zoom,           {0} },
     { MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
     { MODKEY|ShiftMask,             XK_f,      togglefullscr,  {0} },
+
+    { MODKEY,                       XK_grave,  togglescratch,  {.v = scratchpadcmd } },
 
     // gaps
     { MODKEY,                          XK_g,      togglegaps,     {0} },
